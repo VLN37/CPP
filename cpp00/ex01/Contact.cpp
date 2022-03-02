fd438019d153152eaca95c180020d705d1761af0
VLN37 <jofelipe@student.42sp.org.br>
2022-03-02 03:59:37 -0300

    refactor: formatting style

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,18 +6,16 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 23:26:11 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/03/01 23:27:00 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/03/02 03:59:01 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
 Contact::Contact(void) {
-
-	return ;
+	return;
 }
 
 Contact::~Contact(void) {
-
-	return ;
+	return;
 }
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,16 +6,15 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:57:12 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/03/01 23:40:31 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/03/02 03:59:22 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
 #include "Contact.hpp"
+#include "PhoneBook.hpp"
 
-int	main(void)
-{
-	PhoneBook	phonebook;
+int main(void) {
+	PhoneBook phonebook;
 
 	phonebook.run();
 	return (0);
