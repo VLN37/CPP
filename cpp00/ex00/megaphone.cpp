7421d3768e7371c4a4bc606d002e2a954b0f4385
VLN37 <jofelipe@student.42sp.org.br>
2022-03-02 03:08:26 -0300

    tweaks to cpp00ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 17:27:14 by jofelipe          #+#    #+#             */
+/*   Updated: 2022/03/02 03:05:35 by jofelipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	std::string str;
+
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++) {
+		str.append(argv[i]);
+		if (i != argc - 1)
+			str.push_back(' ');
+	}
+	for (std::string::iterator ptr = str.begin(); ptr != str.end(); ptr++)
+		*ptr = toupper(*ptr);
+	std::cout << str << std::endl;
+	return (0);
+}
