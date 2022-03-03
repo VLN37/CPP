364c93481631fc87d4263b1233288022622d36ae
VLN37 <jofelipe@student.42sp.org.br>
2022-03-03 15:49:48 -0300

    cpp00ex01 getters and setters

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 23:26:11 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/03/02 03:59:01 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/03/03 03:43:39 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,48 @@ Contact::Contact(void) {
 	return;
 }
 
+
 Contact::~Contact(void) {
 	return;
 }
+
+std::string Contact::getname(void) const {
+	return (this->name);
+}
+
+std::string Contact::getsurname(void) const {
+	return (this->surname);
+}
+
+std::string Contact::getnickname(void) const {
+	return (this->nickname);
+}
+
+std::string Contact::getphone(void) const {
+	return (this->phone);
+}
+
+std::string Contact::getsecret(void) const {
+	return (this->secret);
+}
+
+void Contact::setname(std::string name) {
+	this->name.assign(name);
+}
+
+void Contact::setsurname(std::string surname) {
+	this->surname.assign(surname);
+}
+
+void Contact::setnickname(std::string nickname) {
+	this->nickname.assign(nickname);
+}
+
+void Contact::setphone(std::string phone) {
+	this->phone.assign(phone);
+}
+
+void Contact::setsecret(std::string secret) {
+	this->secret.assign(secret);
+}
+
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 20:39:56 by jofelipe          #+#    #+#             */
-/*   Updated: 2022/03/02 03:59:15 by jofelipe         ###   ########.fr       */
+/*   Updated: 2022/03/03 04:05:36 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,24 @@ class Contact {
 	public:
 		Contact(void);
 		~Contact(void);
+
+		std::string getname(void) const;
+		std::string getsurname(void) const;
+		std::string getnickname(void) const;
+		std::string getphone(void) const;
+		std::string getsecret(void) const;
+		void		setname(std::string name);
+		void		setsurname(std::string surname);
+		void		setnickname(std::string nickname);
+		void		setphone(std::string phone);
+		void		setsecret(std::string secret);
+
+	private:
 		std::string	name;
 		std::string	surname;
 		std::string	nickname;
 		std::string	phone;
 		std::string	secret;
-
-	private:
 };
 
 #endif
