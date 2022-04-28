d4b61f17ed823e6c74f66b3aee423109e817ee3c
VLN37 <jofelipe@student.42sp.org.br>
2022-04-28 17:52:37 -0300

    cpp07 include intra tests

--- cpp07/ex02_Array/main.cpp
+++ cpp07/ex02_Array/main.cpp
@@ -1,8 +1,10 @@
 // Copyright (c) 2022 João Rodriguez A.K.A. VLN37. All rights reserved.
 
+#include <cstdlib>
 #include "Array.hpp"
 
-int main(void) {
+#define MAX_VAL 750
+int main(int, char**) {
 	std::cout << "this compiles\n";
 	Array<int> arr1(3);
 	Array<int> arr2(3);
@@ -45,5 +47,52 @@ int main(void) {
 		std::cout << "Ha. Ha. Nice try.\n";
 	}
 	delete a;
+
+    // Array<int> numbers(MAX_VAL);
+    // int* mirror = new int[MAX_VAL];
+    // srand(time(NULL));
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     const int value = rand();
+    //     numbers[i] = value;
+    //     mirror[i] = value;
+    // }
+    // //SCOPE
+    // {
+    //     Array<int> tmp = numbers;
+    //     Array<int> test(tmp);
+    // }
+
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     if (mirror[i] != numbers[i])
+    //     {
+    //         std::cerr << "didn't save the same value!!" << std::endl;
+    //         return 1;
+    //     }
+    // }
+    // try
+    // {
+    //     numbers[-2] = 0;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+    // try
+    // {
+    //     numbers[MAX_VAL] = 0;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     numbers[i] = rand();
+    // }
+    // delete [] mirror;//
+
 	return 0;
 }
