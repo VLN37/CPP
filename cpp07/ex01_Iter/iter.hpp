cc0c4419f363d050b352a0d14ca713e334903a5d
VLN37 <jofelipe@student.42sp.org.br>
2022-04-30 16:58:17 -0300

    cpp07ex01 fix to work with const and non const

--- cpp07/ex01_Iter/iter.hpp
+++ cpp07/ex01_Iter/iter.hpp
@@ -9,22 +9,44 @@
 #include <cstdlib>
 
 template<typename T>
-void addOne(T* var) {
-	*var += 1;
+void addOne(T& var) {
+	var += 1;
 }
 
 template<typename T>
 void print_array(T* arr, unsigned int size) {
 	for (size_t i = 0; i < size; i++)
-			std::cout << arr[i] << ' ';
+		std::cout << arr[i] << ' ';
 	std::cout << '\n';
 }
 
 template<typename T>
-void iter(T* arr, unsigned int size, void(*function)(T*)) {
+void iter(T* arr, unsigned int size, void(*function)(T const&)) {
 	for (size_t i = 0; i < size; i++) {
-		function(&arr[i]);
+		function(arr[i]);
 	}
 }
+
+template<typename T>
+void iter(T* arr, unsigned int size, void(*function)(T&)) {
+	for (size_t i = 0; i < size; i++) {
+		function(arr[i]);
+	}
+}
+
+class Awesome
+{
+public:
+Awesome( void ) : _n( 42 ) { return; }
+int get( void ) const { return this->_n; }
+private:
+int _n;
+};
+std::ostream & operator<<(std::ostream & o, Awesome const & rhs)
+{ o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const& x ) { std::cout << x << std::endl; return; }
+
 
 #endif
--- cpp07/ex01_Iter/main.cpp
+++ cpp07/ex01_Iter/main.cpp
@@ -2,7 +2,6 @@
 
 #include "iter.hpp"
 
-
 int main(void) {
 	std::cout << "this compiles\n";
 
@@ -10,25 +9,31 @@ int main(void) {
 	float	arrfloat[] = { 42.21, 21.42, 12.24};
 	int		arrint[] = { 24, 42, 21};
 
-	void (*fint)(int *) = &addOne<int>;
-	void (*fchar)(char *) = &addOne<char>;
-	void (*ffloat)(float *) = &addOne<float>;
-
 	std::cout << "String before     : ";
 	print_array(str, strlen(str));
-	iter(str, 9, fchar);
+	iter(str, 9, addOne<char>);
 	std::cout << "String after      : ";
 	print_array(str, strlen(str));
 
+	std::cout << "Float array before: ";
+	print_array(arrfloat, 3);
+	iter(arrfloat, 3, addOne<float>);
+	std::cout << "Float array after : ";
+	print_array(arrfloat, 3);
+
 	std::cout << "Int array before  : ";
 	print_array(arrint, 3);
-	iter(arrint, 3, fint);
+	iter(arrint, 3, addOne<int>);
 	std::cout << "Int array after   : ";
 	print_array(arrint, 3);
 
-	std::cout << "Float array before: ";
-	print_array(arrfloat, 3);
-	iter(arrfloat, 3, ffloat);
-	std::cout << "Float array after : ";
-	print_array(arrfloat, 3);
+	//PDF script
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter(tab, 5, print);
+	iter(tab2, 5, print);
+
+	return 0;
 }
+
