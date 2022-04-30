97072e86589558265a75ae758d4ccba34aa3af3b
VLN37 <jofelipe@student.42sp.org.br>
2022-04-30 16:59:09 -0300

    cpp07ex01 remove complex types

--- cpp07/ex01_Iter/iter.hpp
+++ cpp07/ex01_Iter/iter.hpp
@@ -34,19 +34,4 @@ void iter(T* arr, unsigned int size, void(*function)(T&)) {
 	}
 }
 
-class Awesome
-{
-public:
-Awesome( void ) : _n( 42 ) { return; }
-int get( void ) const { return this->_n; }
-private:
-int _n;
-};
-std::ostream & operator<<(std::ostream & o, Awesome const & rhs)
-{ o << rhs.get(); return o; }
-
-template< typename T >
-void print( T const& x ) { std::cout << x << std::endl; return; }
-
-
 #endif
--- cpp07/ex01_Iter/main.cpp
+++ cpp07/ex01_Iter/main.cpp
@@ -27,13 +27,6 @@ int main(void) {
 	std::cout << "Int array after   : ";
 	print_array(arrint, 3);
 
-	//PDF script
-	int tab[] = { 0, 1, 2, 3, 4 };
-	Awesome tab2[5];
-
-	iter(tab, 5, print);
-	iter(tab2, 5, print);
-
 	return 0;
 }
 
